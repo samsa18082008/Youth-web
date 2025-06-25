88010819a6961d5d3b56ae28614c9e8d27b4f759
samsa18082008 <yassine.massking@gmail.com>
2025-06-25 12:56:56 +0200

    Add React + Tailwind navigation menu

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+      }
+    }
+  },
+  plugins: [],
+}
